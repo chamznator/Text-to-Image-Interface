7de86135d5b79f9fa44bdca15feddc68ed8f4eea
Chamz <chamz_210@hotmail.com>
2023-03-04 19:25:38 -0500

    add and fix styles of topbar and intro section

--- text-to-image-interface/src/components/introbar/Searchbar.jsx
+++ text-to-image-interface/src/components/introbar/Searchbar.jsx
@@ -7,10 +7,20 @@ export default function Searchbar() {
 return(
 
 
-
+<>
+<div className="introSection">
+        <span className="titleW">
+            Imagen
+        </span>
+        <span className="introMsg">
+            Internet's largest source for AI visual collaboration
+        </span>
+    </div>
 <div class="image-prompt-form-wrapper">
+    
+       
     <div class="image-prompt-form-header"><div>
-        <div class="body-small">Start with a detailed description</div>
+        <div class="body-small">Enter a detailed description,</div>
         <button tabindex="0" class="btn btn-small btn-filled btn-secondary surprise-button" type="button">
             <span class="btn-label-wrap">
                 <span class="btn-label-inner">Surprise me‍</span>
@@ -20,16 +30,19 @@ return(
                 <div>
                     </div>
     </div>
-    <form class="image-prompt-form desktop">
-                        <input type="text" class="text-input text-input-medium text-input-outlined image-prompt-input" placeholder="An Impressionist oil painting of sunflowers in a purple vase…" maxlength="400">
-                            </input>
-                            <button  class="btn btn-disabled btn-disabled-style btn-large btn-filled btn-secondary image-prompt-btn" type="button">
-                                <span class="btn-label-wrap">
-                                    <span class="btn-label-inner">Generate‍</span>
-                                    </span>
-                                    </button>
-    </form>
-    <div class="create-page-upload mt-10 body-small">
+    <div className="formWrapper">
+        <form class="image-prompt-form desktop">
+                            <input type="text" class="text-input text-input-medium text-input-outlined image-prompt-input" placeholder="An Impressionist oil painting of sunflowers in a purple vase…" maxlength="400">
+                                </input>
+                                <button  class="btn btn-disabled btn-disabled-style btn-large btn-filled btn-secondary image-prompt-btn" type="button">
+                                    <span class="btn-label-wrap">
+                                        <span class="btn-label-inner">Generate‍</span>
+                                        </span>
+                                        </button>
+        </form>
+    </div>    
+</div>
+<div class="create-page-upload mt-10 body-small">
         <span>Or, </span>
         <span role="button">
             <span class="link-style inline-link">upload an image</span>
@@ -37,8 +50,7 @@ return(
         <input type="file" accept="image/*" class="file-picker-input"></input>
          <span> to edit</span>
     </div>
-</div>
-
+    </>
 )
 
 
--- text-to-image-interface/src/components/sidebar/Sidebar.jsx
+++ text-to-image-interface/src/components/sidebar/Sidebar.jsx
@@ -9,6 +9,7 @@ import {
   WorkOutline,
   Event,
   School,
+  TagIcon
 } from "@mui/icons-material";
 import { Users } from "../../dummyData";
 
@@ -22,14 +23,7 @@ export default function Sidebar() {
             <RssFeed className="sidebarIcon" />
             <span className="sidebarListItemText">Feed</span>
           </li>
-          <li className="sidebarListItem">
-            <Chat className="sidebarIcon" />
-            <span className="sidebarListItemText">Chats</span>
-          </li>
-          <li className="sidebarListItem">
-            <PlayCircleFilledOutlined className="sidebarIcon" />
-            <span className="sidebarListItemText">Videos</span>
-          </li>
+         
           <li className="sidebarListItem">
             <Group className="sidebarIcon" />
             <span className="sidebarListItemText">Groups</span>
@@ -38,27 +32,76 @@ export default function Sidebar() {
             <Bookmark className="sidebarIcon" />
             <span className="sidebarListItemText">Bookmarks</span>
           </li>
-          <li className="sidebarListItem">
-            <HelpOutline className="sidebarIcon" />
-            <span className="sidebarListItemText">Questions</span>
-          </li>
-          <li className="sidebarListItem">
-            <WorkOutline className="sidebarIcon" />
-            <span className="sidebarListItemText">Jobs</span>
-          </li>
-          <li className="sidebarListItem">
-            <Event className="sidebarIcon" />
-            <span className="sidebarListItemText">Events</span>
-          </li>
-          <li className="sidebarListItem">
-            <School className="sidebarIcon" />
-            <span className="sidebarListItemText">Courses</span>
-          </li>
+       
         </ul>
         <button className="sidebarButton">Show More</button>
         <hr className="sidebarHr" />
+
+        <div className="suggestedWrapper">
+            <p data-e2e="suggest-accounts" class="tiktok-kkg08c-PTitle e4a681b1">Suggested accounts</p>
+            <div className="tiktok-1mo2fkg-DivUserLinkContainer e797se20">
+
+                <img className="sugImg"  src="assets/person/1.jpeg" alt="" />
+                <a href="" className="sugName">
+                    <div className="nameWrapper">
+                        <h4 className="idTag">theweekend</h4>
+                        <p className="nameTag">The weekend</p>
+                    </div>
+            
+                </a>
+
+                
+            </div>
+            <div className="tiktok-1mo2fkg-DivUserLinkContainer e797se20">
+
+                <img className="sugImg"  src="assets/person/2.jpeg" alt="" />
+                <a href="" className="sugName">
+                    <div className="nameWrapper">
+                        <h4 className="idTag">theweekend</h4>
+                        <p className="nameTag">The weekend</p>
+                    </div>
+
+                </a>
+
+
+                </div>
         
-      </div>
+        
+        </div>
+
+
+
+        <hr className="sidebarHr" />
+
+        <div class="tiktok-jvyzjv-DivDiscoverContainer eikhr9j2">
+            <p data-e2e="nav-discover-title" class="tiktok-pvpn9t-PDiscoverTitle eikhr9j3">Discover</p>
+            <div className="trendingWrapper">
+                <div class="tiktok-1avbtpl-DivDiscoverItemContainer eikhr9j7">
+                    <a href=""><span class="tiktok-e841qv-PText eikhr9j12"># whenwomenwin</span>
+                    </a>                          
+                </div>      
+                <div class="tiktok-1avbtpl-DivDiscoverItemContainer eikhr9j7">
+                    <a href=""><span class="tiktok-e841qv-PText eikhr9j12"># blackhistory</span>
+                    </a>                          
+                </div>   
+                <div class="tiktok-1avbtpl-DivDiscoverItemContainer eikhr9j7">
+                    <a href=""><span class="tiktok-e841qv-PText eikhr9j12"># bkdfklad;fjkaldfadjsfljasf;lkdjafjaskl;</span>
+                    </a>                          
+                </div>   
+                <div class="tiktok-1avbtpl-DivDiscoverItemContainer eikhr9j7">
+                    <a href=""><span class="tiktok-e841qv-PText eikhr9j12"># whenwomenwin</span>
+                    </a>                          
+                </div>
+                <div class="tiktok-1avbtpl-DivDiscoverItemContainer eikhr9j7">
+                    <a href=""><span class="tiktok-e841qv-PText eikhr9j12"># whenwomenwin</span>
+                    </a>                          
+                </div>
+            </div>
+                
+        </div>
+    </div>
+        
+      
     </div>
   );
 }
